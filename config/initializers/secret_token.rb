ec2460847589461ec194f1319ee53db5cde6caeb
Michał Łomnicki <michal.lomnicki@gmail.com>
2014-01-31 11:17:05 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lint::Application.config.secret_key_base = 'ca786c8a4fcf9ebb4aa819d15c5e83352a65983d67575c3c15ac0db513a07effb7a56783bde9105f7832d73bd07d59bfa87d49eaf65a15f63c125fab73a9b7cd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lint::Application.config.session_store :cookie_store, key: '_lint_session'
